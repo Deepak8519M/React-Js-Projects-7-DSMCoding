f1ac6baa81a6a462e15f77a01ead787cf3acf061
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-13 18:07:19 +0530

    nav commit

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      
+      <nav>
+        <div className="logo">
+          <img src="http://localhost:5174/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>
+            <a href="">Location</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+        <button>Login</button>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
