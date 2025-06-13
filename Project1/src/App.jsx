3164951dce1ad36000468ccaa208793f32fae498
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-13 18:08:32 +0530

    navigation component created and code pasted

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,29 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="http://localhost:5174/images/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li>
-            <a href="">Location</a>
-          </li>
-          <li>
-            <a href="">Menu</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-        <button>Login</button>
-      </nav>
+      <Navigation />
     </div>
   );
 }
---
+++ Project1/src/components/Navigation.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="http://localhost:5174/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>
+            <a href="">Location</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
