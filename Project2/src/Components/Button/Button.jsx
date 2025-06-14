bd58a961c37423819a6258d2b4d9e52f8f90dc49
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-15 01:18:45 +0530

    additonal code

--- Project2/src/Components/Button/Button.jsx
+++ Project2/src/Components/Button/Button.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { MdMessage } from "react-icons/md";
 import styles from "./Button.module.css";
 
-function Button({ text, icon, isOutline }) {
+function Button({ text, icon, isOutline, ...rest }) {
   return (
-    <button className={isOutline ? styles.outline_btn : styles.primary_btn}>
+    <button
+      {...rest}
+      className={isOutline ? styles.outline_btn : styles.primary_btn}
+    >
       {icon}
       {text}
     </button>
--- Project2/src/Components/ContactForm/ContactForm.jsx
+++ Project2/src/Components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ContactForm.module.css";
 import Button from "../Button/Button";
 import { MdMessage } from "react-icons/md";
@@ -6,6 +6,17 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 function ContactForm() {
+  const [name, setName] = useState("hello");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setName(event.target[0].value);
+    setEmail(event.target[1].value);
+    setText(event.target[2].value);
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.contact_form}>
@@ -15,7 +26,7 @@ function ContactForm() {
         </div>
         <Button isOutline={true} text="VIA EMAIL FORM" icon={<MdEmail />} />
 
-        <form>
+        <form onSubmit={onSubmit}>
           <div className={styles.form_control}>
             <label htmlFor="name">Name</label>
             <input type="text" name="name" />
@@ -31,6 +42,8 @@ function ContactForm() {
           <div style={{ display: "flex", justifyContent: "end" }}>
             <Button text="SUBMIT BUTTON" />
           </div>
+
+          <div>{`${name} ${email} ${text}`}</div>
         </form>
       </div>
       <div className={styles.contact_image}>
