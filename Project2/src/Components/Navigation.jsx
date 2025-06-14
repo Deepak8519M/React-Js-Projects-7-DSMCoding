c0bfbf6e0b56a3869fb420290af24124d7cafb3b
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-14 23:40:20 +0530

    app.css file created

--- Project2/src/Components/Navigation.jsx
+++ Project2/src/Components/Navigation.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Navigation() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav>
+      <div className="logo">
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
