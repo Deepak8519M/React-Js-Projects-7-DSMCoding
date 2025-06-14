28a5cf3da6e5319630099dbcbb8cc8600b55c5b4
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-14 23:57:48 +0530

    CH code comp written

--- Project2/src/App.jsx
+++ Project2/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import "./App.css"; // Assuming you have some styles in App.css
+import ContactHeader from "./Components/ContactHeader/ContactHeader";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <ContactHeader />
     </div>
   );
 }
