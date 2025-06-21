44c820dcbfa3e00f2bc0d5a08b932bb0f1a80d0e
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-21 17:37:11 +0530

    effective technq code for box pushed

--- Project3/src/components/GamePlay.jsx
+++ Project3/src/components/GamePlay.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 import TotalScore from "./TotalScore";
+import NumberSelector from "./NumberSelector";
 
 function GamePlay() {
   return (
     <main>
       <TotalScore />
+      <NumberSelector />
     </main>
   );
 }
 
 export default GamePlay;
-
-
--- Project3/src/components/NumberSelector.jsx
+++ Project3/src/components/NumberSelector.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import styled from "styled-components";
 
 function NumberSelector() {
+  const arrayNumber = [1, 2, 3, 4, 5, 6];
+
   return (
     <div>
-      
+      {arrayNumber.map((value, i) => (
+        <Box key={i}>{value}</Box>
+      ))}
     </div>
-  )
+  );
 }
 
-export default NumberSelector
+export default NumberSelector;
+
+const Box = styled.div`
+  height: 72px;
+  width: 72px;
+  border: 1px solid black;
+  display: grid;
+  place-items: center;
+  font-weight: 700;
+  font-size: 24px;
+`;
