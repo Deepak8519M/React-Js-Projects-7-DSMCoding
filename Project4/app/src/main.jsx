0b1540c96ef0698ef3f33f169d31e59a8c768cf5
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-26 10:35:51 +0530

    globals style added

--- Project4/app/src/main.jsx
+++ Project4/app/src/main.jsx
@@ -9,11 +9,18 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: "Urbanist",sans-serif
+
+  }
+  body{
+    background: #323334;
+    color: white;
+    min-height: 100vh;
   }
 `;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
